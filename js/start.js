092c3f85a608b7c559a438de2a4a16077739ab73
Dipayan <stylishbittu18@gmail.com>
2021-07-15 20:14:55 +0530

    Second Commit

--- js/start.js
+++
@@ -1,7 +0,0 @@
-function submitForm(event) {
-	event.preventDefault();
-	let name = document.getElementById('exampleFormControlTextarea1').value;
-	sessionStorage.setItem('name', name);
-	location.href = '/index.html';
-	console.log(name);
-}
